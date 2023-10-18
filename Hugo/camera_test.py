22eaa8bed215370b2210cdbc4aa7c5afc7663b0d
Hugo Berg <hugo@hugob.se>
2023-10-18 03:03:28 +0100

    Camera & oled tests working

--- Hugo/camera_test.py
+++ Hugo/camera_test.py
@@ -1,4 +1,4 @@
-from picamera2.encoders import MJPEGEncoder, Quality
+from picamera2.encoders import H264Encoder, Quality
 from picamera2.outputs import FfmpegOutput
 from picamera2 import Picamera2
 import time
@@ -8,12 +8,14 @@ video_config = picam2.create_video_configuration()
 picam2.configure(video_config)
 print("Created camera")
 
-encoder = MJPEGEncoder(bitrate=int(5e7))
+encoder = H264Encoder(bitrate=int(2e7))
 output = FfmpegOutput("test.mp4")
 
 print("Starting recording")
 picam2.start_recording(encoder, output)
-time.sleep(10)
+for i in range(20):
+    time.sleep(1)
+    print(f"t={i}")
 print("Stopping recording")
 picam2.stop_recording()
 print("Done!")
