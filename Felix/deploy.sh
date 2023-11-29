c68d11bb9a4267e30bcb2f0bcab24d00a215e425
Hugo Berg <hugo@gpuevm.tech>
2023-11-29 21:20:58 +0000

    fix

--- Felix/deploy.sh
+++ Felix/deploy.sh
@@ -10,7 +10,7 @@ scp -r scripts $MISSION_USERNAME@$MISSION_HOSTNAME:~/mission/
 
 (cd detect-system-failure || exit 1;
   cross build --release --target aarch64-unknown-linux-gnu
-  scp -r ./target/aarch64-unknown-linux-gnu/release/detect-system-failure $MISSION_HOSTNAME@$MISSION_HOSTNAME:~/mission/scripts/detect_system_failure  
+  scp -r ./target/aarch64-unknown-linux-gnu/release/detect-system-failure $MISSION_USERNAME@$MISSION_HOSTNAME:~/mission/scripts/detect_system_failure  
 )
 
 scp -r media $MISSION_USERNAME@$MISSION_HOSTNAME:~/mission/
